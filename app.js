5ad7e02ddc0cb0510a31e5ae7119fc7e23b0672a
Clément Devos <cdevos.0@gmail.com>
2019-09-25 17:25:02 +0200

    on teste de l'optim?

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const { convert } = require('convert-svg-to-png');
-
+process.setMaxListeners(0);
 // React Components
 import React from 'react';
 import RDS from 'react-dom/server';
@@ -8,27 +8,41 @@ import Avataaars from 'avataaars';
 
 const app = express();
 
+const pngCache = {}
+app.use(function(req, res, next) {
+  console.log(`[${req.method}][${new Date().getTime()}]${req.path}`)
+  next();
+});
 app.get('/', async(req, res) => {
   const appString = RDS.renderToString(<Avataaars {...req.query} />);
 
-  res.writeHead(200, {
-    'Content-Type': 'image/svg+xml',
-  });
-  res.end(appString);
+  res.set('Content-Type', 'image/svg+xml');
+  res.send(appString);
 });
 
 app.get('/png/:width?', async(req, res) => {
+  const {width :widthParam = "500"} = req.params;
+  const width =parseInt(widthParam, 10);
+
+  if(pngCache[req.originalUrl]){
+    console.log('Returning cached value for' , req.originalUrl)
+    res.set('Content-Type', 'image/png');
+    res.send(pngCache[req.originalUrl]);
+    return;
+  }
+
   const appString = RDS.renderToString(<Avataaars {...req.query} />);
 
   const png = await convert(appString, {
-    width: parseInt(req.params.width || 500, 10),
+    width,
     puppeteer: {
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
   });
+  pngCache[req.originalUrl] =  png;
 
   res.set('Content-Type', 'image/png');
-  res.end(png);
+  res.send(png);
 });
 
 // catch 404 and forward to error handler
